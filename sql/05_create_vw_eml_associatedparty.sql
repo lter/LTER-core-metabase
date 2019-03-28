d9aa6c1b9a517a0f8f06c8b03f94041b50e1d14c
Margaret O'Brien <margaret.obrien@ucsb.edu>
2019-03-28 09:35:32 -0700

    added the left join to people identifier table

--- sql/05_create_vw_eml_associatedparty.sql
+++ sql/05_create_vw_eml_associatedparty.sql
@@ -6,15 +6,17 @@
 
 CREATE OR REPLACE VIEW mb2eml_r.vw_eml_associatedparty AS
  SELECT d."DataSetID" AS datasetid, d."AuthorshipOrder" AS authorshiporder, 
-    d."NameID" AS nameid, p."GivenName"::text AS givenname, 
-    p."MiddleName" AS givenname2, p."SurName" AS surname, 
-    p."Organization" AS organization, p."Address1" AS address1, 
-    p."Address2" AS address2, p."Address3" AS address3, p."City" AS city, 
-    p."State" AS state, p."Country" AS country, p."ZipCode" AS zipcode, 
-    p."Phone1" AS phone1, p."Phone2" AS phone2, p."FAX" AS fax, 
-    p."Email" AS email, p."WebPage" AS onlineurl, d."AuthorshipRole" AS role
+    d."AuthorshipRole" AS authorshiprole, d."NameID" AS nameid, 
+    p."GivenName"::text AS givenname, p."MiddleName" AS givenname2, 
+    p."SurName" AS surname, p."Organization" AS organization, 
+    p."Address1" AS address1, p."Address2" AS address2, 
+    p."Address3" AS address3, p."City" AS city, p."State" AS state, 
+    p."Country" AS country, p."ZipCode" AS zipcode, p."Phone1" AS phone1, 
+    p."Phone2" AS phone2, p."FAX" AS fax, p."Email" AS email, 
+    i."Identificationlink" AS orcid
    FROM lter_metabase."DataSetPersonnel" d
    LEFT JOIN lter_metabase."People" p ON d."NameID"::text = p."NameID"::text
+   LEFT JOIN lter_metabase."Peopleidentification" i ON d."NameID"::text = i."NameID"::text
   WHERE d."AuthorshipRole"::text <> ALL (ARRAY['creator'::text, 'metadataProvider'::text, 'contact'::text, 'publisher'::text])
   ORDER BY d."DataSetID", d."AuthorshipOrder";
 
