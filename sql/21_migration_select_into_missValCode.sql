997c9fdadcaf0bab4803adee950cbc3cb8097a63
gastil <gastil-buhl@ucsb.edu>
2019-07-23 16:07:27 -0700

    select-into miss val codes from att table to normalized parent and xref tables. Also uniq on parent table.

--- sql/21_migration_select_into_missValCode.sql
+++ sql/21_migration_select_into_missValCode.sql
@@ -4,62 +4,60 @@
  *
  * (in postgres, select into is actually insert from select. Same thing.)
  * 
- * Only for migration purposes, the old DataSetAttributes column MissingValueCode and "Explanation 
- * were left behind in their 1-to-1 way. 
- * The metabase user should opt either all one or all the other way, not mix 1-1 and n-n.
- * Either use the normalized tables and not populate the deprecated columns,
- * or use only the deprecated columns.
+ * Only for migration purposes, the old DataSetAttributes column MissingValueCode and "Explanation" 
+ * here are used to populate the new ListMissingCodes parent table and 
+ * the cross-ref table DataSetAttributeMissingCodes.
+ * 
+ * At least initially, an integer ID is generated. 
+ * The ID field is actually a varchar(20) to allow meaningful strings for human editors, if you so wish.
+ * Just edit them in the parent table and the ID will cascade.
+ * This is NOT an "autoinc". This is just for initial population.
+ * And there is a unique constraint on the other 2 columns so no risk of duplicate tuples.
  * 
 */
 
--- Exploratory queries
+-- special for mini_sbclter: m is lowercase in col name "missingValueCodeExplanation"
+-- for mini_sbclter, edit this script accordingly.
 
-select "DataSetID", "EntitySortOrder", "ColumnPosition", 
- "MissingValueCode", "MissingValueCodeExplanation"
- from lter_metabase."DataSetAttributes" a
- ORDER BY "MissingValueCode", "DataSetID", "EntitySortOrder", "ColumnPosition"
- ;
- 
- select "DataSetID", "EntitySortOrder", "ColumnName", 
- "MissingValueCode", "MissingValueCodeExplanation"
- from lter_metabase."DataSetAttributes" a
- ORDER BY "MissingValueCode", "DataSetID", "EntitySortOrder", "ColumnPosition"
- ;
- 
- -- summarize re-use of mv codes
- 
- select count(*) as times_used, 
-	count(distinct "DataSetID") as ds_using, -- "EntitySortOrder", "ColumnPosition", 
- "MissingValueCode", "MissingValueCodeExplanation"
- from lter_metabase."DataSetAttributes" a
+-- add a uniq constraint to code and explain columns of parent table.
+ALTER TABLE lter_metabase."ListMissingCodes"
+  ADD CONSTRAINT "UQ_ListMissingCodes_Code_Explanation" UNIQUE ("MissingValueCode", "MissingValueCodeExplanation");
+COMMENT ON CONSTRAINT "UQ_ListMissingCodes_Code_Explanation" ON lter_metabase."ListMissingCodes"
+  IS 'Needed because the ID could be as simple as an integer and is not inherently connected to the code and explanation.';
+
+
+-- populate parent table
+
+create temporary sequence id_mvc increment by 1 minvalue 1 start with 1 ;
+
+INSERT INTO lter_metabase."ListMissingCodes" ("MissingValueCodeID","MissingValueCode", "MissingValueCodeExplanation")
+(
+SELECT  
+ nextval('id_mvc'), -- gets next integer from sequence  
+ "MissingValueCode",
+ "MissingValueCodeExplanation" 
+ FROM lter_metabase."DataSetAttributes" 
+ WHERE "MissingValueCode" is not null
  GROUP BY "MissingValueCode", "MissingValueCodeExplanation"
- ORDER BY "MissingValueCode", "MissingValueCodeExplanation"
- ;
+);
+
+INSERT INTO lter_metabase."DataSetAttributeMissingCodes" ("DataSetID", "EntitySortOrder", "ColumnName", "MissingValueCodeID")
+(
+SELECT  "DataSetID", "EntitySortOrder", "ColumnName", c."MissingValueCodeID"
+ from lter_metabase."DataSetAttributes" a
+ join lter_metabase."ListMissingCodes" c
+ on a."MissingValueCode"=c."MissingValueCode" and a."MissingValueCodeExplanation"=c."MissingValueCodeExplanation"
+ where a."MissingValueCode" is not null
+ );
  
- select "MissingValueCode" as "MissingValueCodeID", 
- "MissingValueCode", "MissingValueCodeExplanation"
+-- Exploratory queries (only run if you just want to see the process)
+
+/*select "DataSetID", "EntitySortOrder", "ColumnName", c."MissingValueCodeID",
+ a."MissingValueCode", a."MissingValueCodeExplanation"
  from lter_metabase."DataSetAttributes" a
- ORDER BY "MissingValueCode"
- ;
- -- mini_sbclter has explainations of length up to 59 char. Need to expand varchar 
- -- if using code||explaination as the ID. Otherwise, how to give ID?
- -- Same code is often used with several explainations.
- select count(*), 
- "MissingValueCode", "missingValueCodeExplanation", LENGTH("missingValueCodeExplanation")
- from lter_metabase."DataSetAttributes" a
- GROUP BY "MissingValueCode", "missingValueCodeExplanation"
- ORDER BY "MissingValueCode", "missingValueCodeExplanation"
- ;
- select count(*), 
- "MissingValueCode", "missingValueCodeExplanation", LENGTH("missingValueCodeExplanation")
- from lter_metabase."DataSetAttributes" a
- GROUP BY "MissingValueCode", "missingValueCodeExplanation"
- ORDER BY LENGTH("missingValueCodeExplanation"), "MissingValueCode", "missingValueCodeExplanation"
- ;
- select count(*), 
- "MissingValueCode", "missingValueCodeExplanation", LENGTH("missingValueCodeExplanation")
- from lter_metabase."DataSetAttributes" a
- GROUP BY "MissingValueCode", "missingValueCodeExplanation"
- ORDER BY "missingValueCodeExplanation", "MissingValueCode"
- ;
+ join lter_metabase."ListMissingCodes" c
+ on a."MissingValueCode"=c."MissingValueCode" and a."MissingValueCodeExplanation"=c."MissingValueCodeExplanation"
+ where a."MissingValueCode" is not null
+ ORDER BY a."MissingValueCode", "DataSetID", "EntitySortOrder", "ColumnPosition"
+ ;*/
  
