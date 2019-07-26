acad72b3f34b2cc7ddf69162b5e3e43c9f6aa510
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-07-26 16:10:17 -0500

    patch22: add GRANT statements

--- sql/22_version_tracker.sql
+++ sql/22_version_tracker.sql
@@ -11,6 +11,11 @@ CREATE TABLE pkg_mgmt.version_tracker_metabase (
 ALTER TABLE pkg_mgmt.version_tracker_metabase
   ADD CONSTRAINT "PK_version_tracker_metabase" PRIMARY KEY (major_version, minor_version, patch);
 
+ ALTER TABLE pkg_mgmt.version_tracker_metabase OWNER TO %db_owner%;
+GRANT SELECT ON TABLE pkg_mgmt.version_tracker_metabase TO read_write_user;
+GRANT SELECT ON TABLE pkg_mgmt.version_tracker_metabase TO read_only_user;
+GRANT ALL ON TABLE pkg_mgmt.version_tracker_metabase TO %db_owner%;
+
 INSERT INTO pkg_mgmt.version_tracker_metabase
 (major_version, minor_version, patch, date_installed, "comment")
 VALUES(0, 9, 22, now(), 'apply 22_version_tracker');
