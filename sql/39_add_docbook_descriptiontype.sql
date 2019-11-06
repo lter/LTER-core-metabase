8c69107255547301a3769d4cc7b62b18c4140fe4
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-11-06 10:19:15 -0600

    patch39: add docbook to description/abstract type constraint, issue #90

---
+++ sql/39_add_docbook_descriptiontype.sql
@@ -0,0 +1,27 @@
+-- This patch addresses issue #90
+
+-- Allows 'docbook' to be specified as a DescriptionType/AbstractType
+
+ALTER TABLE lter_metabase."DataSetMethodSteps"
+    DROP CONSTRAINT "CK_DataSetMethodSteps_DescriptionType";
+
+ALTER TABLE lter_metabase."DataSetMethodSteps"
+    ADD CONSTRAINT "CK_DataSetMethodSteps_DescriptionType" 
+    CHECK ((("DescriptionType")::text = ANY (ARRAY[('file'::character varying)::text, 
+        ('md'::character varying)::text, 
+        ('plaintext'::character varying)::text,
+        ('docbook'::character varying)::text])));
+
+ALTER TABLE lter_metabase."DataSet"
+    DROP CONSTRAINT "CK_DataSet_AbstractType";
+
+ALTER TABLE lter_metabase."DataSet"
+    ADD CONSTRAINT "CK_DataSetMethodSteps_AbstractType" 
+    CHECK ((("AbstractType")::text = ANY (ARRAY[('file'::character varying)::text, 
+        ('md'::character varying)::text, 
+        ('plaintext'::character varying)::text,
+        ('docbook'::character varying)::text])));
+
+INSERT INTO pkg_mgmt.version_tracker_metabase
+(major_version, minor_version, patch, date_installed, "comment")
+VALUES(1, 0, 39, now(), 'apply 39_add_docbook_descriptiontype.sql');
