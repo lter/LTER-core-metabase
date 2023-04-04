293947007ae5faca78130b22b18d2b08b9db8eac
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2023-04-04 13:23:02 -0500

    add 43_move_annotation_tables.sql

---
+++ sql/43_move_annotation_tables.sql
@@ -0,0 +1,19 @@
+ALTER TABLE if exists semantic_annotation."DataSetAnnotation"
+    SET SCHEMA lter_metabase;
+
+ALTER TABLE IF EXISTS semantic_annotation."DataSetAttributeAnnotation"
+    SET SCHEMA lter_metabase;
+
+ALTER TABLE IF EXISTS semantic_annotation."EMLObjectProperties"
+    SET SCHEMA lter_metabase;
+
+ALTER TABLE IF EXISTS semantic_annotation."EMLSemanticAnnotationTerms"
+    SET SCHEMA lter_metabase;
+
+ALTER TABLE IF EXISTS lter_metabase."EMLSemanticAnnotationTerms" RENAME TO "ListSemanticAnnotationTerms";
+
+DROP SCHEMA semantic_annotation;
+
+INSERT INTO pkg_mgmt.version_tracker_metabase
+(major_version, minor_version, patch, date_installed, "comment")
+VALUES(1, 0, 43, now(), 'apply 43_move_annotation_tables.sql');
