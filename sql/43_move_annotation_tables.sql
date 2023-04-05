f805438cf64a5c28722a042cf5b5ea6619ae9b67
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2023-04-05 14:11:06 -0500

    alter patch 43 to also move the semantic annotation view

--- sql/43_move_annotation_tables.sql
+++ sql/43_move_annotation_tables.sql
@@ -12,6 +12,8 @@ ALTER TABLE IF EXISTS semantic_annotation."EMLSemanticAnnotationTerms"
 
 ALTER TABLE IF EXISTS lter_metabase."EMLSemanticAnnotationTerms" RENAME TO "ListSemanticAnnotationTerms";
 
+ALTER VIEW IF EXISTS semantic_annotation.vw_eml_semantic_annotation SET SCHEMA mb2eml_r;
+
 DROP SCHEMA semantic_annotation;
 
 INSERT INTO pkg_mgmt.version_tracker_metabase
